fcfb000848c5a657bfd77262eeede02e089670f3
Howard1209 <howardjobinfo@gmail.com>
2023-07-27 19:34:58 +0800

    add rate limit

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, Router } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from "path";
+import rateLimit from 'express-rate-limit'
 import backtestingRouter from './routes/backtesting.js';
 import userRouter from "./routes/user.js";
 import webScrapingRouter from "./routes/webScraping.js"
@@ -10,10 +11,16 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 30,
+	standardHeaders: true,
+	legacyHeaders: false,
+})
 
 app.use(cors());
 app.use(express.json());
-app.use("/api",[
+app.use("/api", apiLimiter,[
   backtestingRouter,
   userRouter,
   webScrapingRouter
